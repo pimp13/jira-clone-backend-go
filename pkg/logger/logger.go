70065f9cd35341d0d28d9b3811366162d3795017
pimp13 <pimp.puma.13@gmail.com>
2025-12-27 17:16:11 +0330

    Refactor logger initialization in the logger package. Updated the New function to correctly assign the logger variable and added caller information for enhanced logging context. Commented out an alternative logger initialization method for future reference.

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -11,6 +11,7 @@ type Logger = *zerolog.Logger
 
 func New(isProduction bool) Logger {
 	var output io.Writer
+	var logger zerolog.Logger
 
 	if isProduction {
 		output = os.Stdout
@@ -22,12 +23,33 @@ func New(isProduction bool) Logger {
 		}
 	}
 
-	logger := zerolog.New(output).
+	logger = zerolog.New(output).
 		Level(zerolog.DebugLevel). // یا InfoLevel یا از env بخون
 		With().
 		Timestamp().
+		Caller().
 		Str("loc", "your-app").
 		Logger()
 
 	return &logger
 }
+
+// func New(loc string) Logger {
+// 	var output io.Writer
+
+// 	output = zerolog.ConsoleWriter{
+// 		Out:        os.Stderr,
+// 		TimeFormat: "15:04:05.000",
+// 		NoColor:    false,
+// 	}
+
+// 	logger := zerolog.New(output).
+// 		Level(zerolog.DebugLevel). // یا InfoLevel یا از env بخون
+// 		With().
+// 		Timestamp().
+// 		Caller().
+// 		Str("loc", loc).
+// 		Logger()
+
+// 	return &logger
+// }
